c842c8c6e39f949e2aa940ef49e1e267fae1adc5
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-12-14 07:50:17 +0530

    Update firebase.ts

--- project/src/config/firebase.ts
+++ project/src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHrGGzzc2WYWmNm3ri9npQ35PUepCMtWE",
-  authDomain: "bringit-5a511.firebaseapp.com",
-  projectId: "bringit-5a511",
-  storageBucket: "bringit-5a511.firebasestorage.app",
-  messagingSenderId: "886136030339",
-  appId: "1:886136030339:web:d8094d21a6e6abfea387b6",
-  measurementId: "G-JWRX1T2VDZ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
